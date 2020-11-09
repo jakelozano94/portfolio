c1b77730c458f68ff153b5831d519384693da092
github username <github email address>
2020-11-09 03:51:04 -0500

    unformatted. adding information

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Skills from './Components/Skills.js'
+
+const aboutMe = "Full Stack Software Engineer with BS in Cellular and Molecular Biology and 6 years of high end kitchen experience at Eleven Madison Park (San Pelligrino’s Best Restaurant in the World). Thrive in environments that are high pressure, reward independence, and have high quality craftsmanship.  Goal oriented and driven to master large scale problems by breaking them into digestible chunks, implementing the plan, and then triaging and solving emerging problems. Passionate about working independently and with a team to manipulate and use data in unintuitive ways to solve real world problems and create meaningful web and data applications to help people."
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="sidenav">
+        <div id="links">
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+      <div className="content">
+        <div className="header">
+            <div id="profile_pic">
+              <img src="https://i.imgur.com/zglnxly.png"/>
+            </div>
+            <h1>Hi! im baby</h1>
+        </div>
+        <div className="break"></div>
+        <div className ="about_me">
+          {aboutMe}
+        </div>
+          <Skills/>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Skills.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Skills() {
+
+    const profs = ["Ruby", "Rails", "Javascript", "React", "Redux", "CSS", "HTML", "Git Workflow", "Bootstrap"]
+    const expos = ["PostgreSQL", "TensorFlow (Core & .js)", "R", "Python"]
+
+    function lister(list){
+        return list.map(skill =>
+            <li>{skill}</li>
+            )
+    }
+
+    return (
+        <div className = "skills">
+            <div id="prof_title"> Proficient: </div>
+            <div className = "prof">
+                <div>
+                    <ul className="dashed">
+                        {lister(profs)}
+                    </ul>
+                </div>
+            </div>
+            <div id="expo_title"> Exposed: </div>
+            <div className = "exposed">
+                <div>
+                    <ul className ="dashed">
+                        {lister(expos)}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
