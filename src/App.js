625220b3fddd644a0391b7840b90c61e564b4008
jakelozano94 <jakelozano94@gmail.com>
2021-01-04 10:45:42 -0500

    added html social buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Skills from './Components/Skills.js'
 import Projects from './Components/Projects'
 import Blogs from './Components/Blogs'
+import Social from './Components/Social'
 
 const aboutMe = "Full Stack Software Engineer with BS in Cellular and Molecular Biology and 6 years of high end kitchen experience, including at Eleven Madison Park (San Pelligrino’s Best Restaurant in the World). Thrive in environments that are high pressure, reward independence, and have high quality craftsmanship.  Goal oriented and driven to master large scale problems by breaking them into digestible chunks, implementing the plan, and then triaging and solving emerging problems. Passionate about working independently and with a team to manipulate and use data in unintuitive ways to solve real world problems and create meaningful web and data applications to help people."
 
