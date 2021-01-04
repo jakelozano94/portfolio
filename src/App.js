2b88395ff7d51b8cf62e45df2ae2fd21f30b7f7f
jakelozano94 <jakelozano94@gmail.com>
2021-01-04 10:57:13 -0500

    got css and socials working together, fixed sass version error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import './App.scss'
 import Skills from './Components/Skills.js'
 import Projects from './Components/Projects'
 import Blogs from './Components/Blogs'
 import Social from './Components/Social'
+
 
 const aboutMe = "Full Stack Software Engineer with BS in Cellular and Molecular Biology and 6 years of high end kitchen experience, including at Eleven Madison Park (San Pelligrino’s Best Restaurant in the World). Thrive in environments that are high pressure, reward independence, and have high quality craftsmanship.  Goal oriented and driven to master large scale problems by breaking them into digestible chunks, implementing the plan, and then triaging and solving emerging problems. Passionate about working independently and with a team to manipulate and use data in unintuitive ways to solve real world problems and create meaningful web and data applications to help people."
 
@@ -15,12 +17,6 @@ function App() {
               <img src="https://i.imgur.com/zglnxly.png"/>
             </div>
             <h1 id="sidebar_name"> Jake Lozano</h1>
-        <div id="links">
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
-          <a href="#">Contact</a>
-        </div>
       </div>
       <div className="content">
         <div className="header">
@@ -34,6 +30,7 @@ function App() {
           <Projects/>
           <Blogs/>
       </div>
+      <Social/>
        
     </>
   );
